bionic-linux/android_frameworks_base

[IT4.2] Update argument type of interfaceClassDataActivityChanged
The argument type of interfaceClassDataActivityChanged takes a
string for the network type. It requires both the receivers and
NMS to do type transformation. The transformation is a redundant
work. Update it to take integer directly and rename to
understandable naming.

Bug: 170598012
Test: atest FrameworksNetTests
Change-Id: Ibe9fa7a1b71af2dab916b5d615742e77e4174c39

core/java/android/net/INetworkManagementEventObserver.aidl
@@ -85,14 +85,14 @@ oneway interface INetworkManagementEventObserver {
     /**
      * Interface data activity status is changed.
      *
-     * @param label Unique identifier indicates the network type of the data activity change.
+     * @param tyep The leagcy network type of the data activity change.
      * @param active  True if the interface is actively transmitting data, false if it is idle.
      * @param tsNanos Elapsed realtime in nanos when the state of the network interface changed.
      * @param uid Uid of this event. It represents the uid that was responsible for waking the
      *            radio. For those events that are reported by system itself, not from specific uid,
      *            use -1 for the events which means no uid.
      */
-    void interfaceClassDataActivityChanged(String label, boolean active, long tsNanos, int uid);
+    void interfaceClassDataActivityChanged(int type, boolean active, long tsNanos, int uid);
 
     /**
      * Information about available DNS servers has been received.

core/java/com/android/server/net/BaseNetworkObserver.java
@@ -64,7 +64,7 @@ public class BaseNetworkObserver extends INetworkManagementEventObserver.Stub {
     }
 
     @Override
-    public void interfaceClassDataActivityChanged(String label, boolean active, long tsNanos,
+    public void interfaceClassDataActivityChanged(int type, boolean active, long tsNanos,
             int uid) {
         // default no-op
     }

SphinxOS/frameworks_base

Former-commit-id: 4dd4e08c4e729672b0056ff938bac2048cc08618

core/java/android/net/INetworkManagementEventObserver.aidl
@@ -85,14 +85,14 @@ oneway interface INetworkManagementEventObserver {
     /**
      * Interface data activity status is changed.
      *
-     * @param label Unique identifier indicates the network type of the data activity change.
+     * @param networkType The legacy network type of the data activity change.
      * @param active  True if the interface is actively transmitting data, false if it is idle.
      * @param tsNanos Elapsed realtime in nanos when the state of the network interface changed.
      * @param uid Uid of this event. It represents the uid that was responsible for waking the
      *            radio. For those events that are reported by system itself, not from specific uid,
      *            use -1 for the events which means no uid.
      */
-    void interfaceClassDataActivityChanged(String label, boolean active, long tsNanos, int uid);
+    void interfaceClassDataActivityChanged(int networkType, boolean active, long tsNanos, int uid);
 
     /**
      * Information about available DNS servers has been received.

core/java/com/android/server/net/BaseNetworkObserver.java
@@ -64,7 +64,7 @@ public class BaseNetworkObserver extends INetworkManagementEventObserver.Stub {
     }
 
     @Override
-    public void interfaceClassDataActivityChanged(String label, boolean active, long tsNanos,
+    public void interfaceClassDataActivityChanged(int networkType, boolean active, long tsNanos,
             int uid) {
         // default no-op
     }